fe4be495b3941dc1998596ed5cc955e7bd911b95
Young-Andrew <76223746+Young-Andrew@users.noreply.github.com>
2021-02-09 13:48:53 -0700

    Add files via upload

---
+++ 01-java-fundamentals/01-first-java-program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Andy");
+        System.out.println("I am 50");
+        System.out.println("I was born in San Francisco");
+    }
+}
