8f37ff278dddd85daef5236d476379126af1e1d7
Young-Andrew <76223746+Young-Andrew@users.noreply.github.com>
2021-02-09 14:14:26 -0700

    Delete FirstJavaProgram.java

--- 01-java-fundamentals/01-first-java-program/FirstJavaProgram.java
+++
@@ -1,7 +0,0 @@
-public class FirstJavaProgram {
-    public static void main(String[] args) {
-        System.out.println("My name is Andy");
-        System.out.println("I am 50");
-        System.out.println("I was born in San Francisco");
-    }
-}
