3767889a82e1ad3d7c9db4b80b4aa40ada047a51
Young-Andrew <restorativecare@gmail.com>
2021-02-10 12:55:25 -0700

    Second Assignment

---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanRun.java
@@ -0,0 +1,7 @@
+public class PythagoreanRun {
+    public static void main(String[] args) {
+        Pythagroean iD = new Pythagroean();
+        Double calcHypot = iD.calculateHypotenuse(4, 6);
+        System.out.println(calcHypot);
+    }
+}
---
+++ 01-java-fundamentals/02-pythagorean-theorem/Pythagroean.java
@@ -0,0 +1,9 @@
+import java.util.Math;
+
+public class Pythagroean {
+    public double calculateHypotenuse(int legA, int legB) {
+        Double hyp = Math.hypot(legA, legB);
+        return hyp;
+    }
+
+}
